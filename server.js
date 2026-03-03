d70f8f455445830a742d0900709d62bd6dab92b3
Kevin Rivas <kevinr060799@gmail.com>
2026-03-02 22:25:05 -0500

    Se modifica el server

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://encuestaclinivision.vercel.app",
+  methods: ["GET", "POST", "OPTIONS"],
+}));
+
 app.use(express.json());
 
-// Rutas
 app.use('/api/encuestas', require('./src/routes/encuesta.routes'));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Base de datos conectada'))
   .catch((err) => console.log('Error conectando DB:', err));
 
-app.listen(4000, () => {
-  console.log('Servidor corriendo en puerto 4000');
-});
+module.exports = app;
